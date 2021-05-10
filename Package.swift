78797395aa7dc97c780ad671e7fdcecbb3b5cefd
Kevin Mullins <kevin_mullins@appracatappra.com>
2021-05-10 10:26:01 -0500

    Fixed Documentation
    Added fixes for documentation to remove it from the build targets.

--- Package.swift
+++ Package.swift
@@ -21,7 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftletUtilities",
-            dependencies: []),
+            dependencies: [],
+            exclude:["Documentation"]),
         .testTarget(
             name: "SwiftletUtilitiesTests",
             dependencies: ["SwiftletUtilities"]),
