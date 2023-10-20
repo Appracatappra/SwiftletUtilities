0d8053e4e4d62f59402e1b23c4d49112a534ffb6
Kevin Mullins <kevin_mullins@appracatappra.com>
2023-10-20 08:12:11 -0500

    Added NSImage Resize
    Added NSImage Resize

--- Sources/SwiftletUtilities/macOS/Extensions/macOS ImageExtensions.swift
+++ Sources/SwiftletUtilities/macOS/Extensions/macOS ImageExtensions.swift
@@ -10,6 +10,7 @@
 import Foundation
 import SwiftUI
 import AppKit
+import LogManager
 
 /**
  Extends `NSBitmapImageRep` to support the Action Data controls and adds convenience methods for working with image properties in a `Codable`, `Encodable` or `Decodable` class.
@@ -204,8 +205,276 @@ extension NSImage {
      
      - Parameter value: A Base 64 encoded `String` representing an image.
      */
-    public convenience init?(named: String) {
-        self.init(named: NSImage.Name(named))
+//    public convenience init?(named: String) {
+//        self.init(named: NSImage.Name(named))
+//    }
+    
+    /// Function for resizing an NSImage.
+    /// - Parameters:
+    ///   - image: The NSImage to resize.
+    ///   - maxSize: The maximum Size for the Image.
+    /// - Returns: The resized Image
+    public static func resizeImage(image:NSImage, maxSize:NSSize) -> NSImage {
+        var ratio:Float = 0.0
+        let imageWidth = Float(image.size.width)
+        let imageHeight = Float(image.size.height)
+        let maxWidth = Float(maxSize.width)
+        let maxHeight = Float(maxSize.height)
+        
+        // Get ratio (landscape or portrait)
+        if (imageWidth > imageHeight) {
+            // Landscape
+            ratio = maxWidth / imageWidth;
+        }
+        else {
+            // Portrait
+            ratio = maxHeight / imageHeight;
+        }
+        
+        // Calculate new size based on the ratio
+        let newWidth = imageWidth * ratio
+        let newHeight = imageHeight * ratio
+        
+        // Create a new NSSize object with the newly calculated size
+        let newSize:NSSize = NSSize(width: Int(newWidth), height: Int(newHeight))
+        
+        // Cast the NSImage to a CGImage
+        var imageRect:CGRect = CGRectMake(0, 0, image.size.width, image.size.height)
+        let imageRef = image.cgImage(forProposedRect: &imageRect, context: nil, hints: nil)
+        
+        // Create NSImage from the CGImage using the new size
+        let imageWithNewSize = NSImage(cgImage: imageRef!, size: newSize)
+        
+        // Return the new image
+        return imageWithNewSize
+    }
+    
+    /// Returns a `NSImage` from an Asset Catalog scaled to the requested size by the given scale factor.
+    /// - Parameters:
+    ///   - named: The name of the image asset to load.
+    ///   - atScale: [Optional] The requested scale for the image. The default is `1.0`.
+    /// - Returns: The requested image asset scaled to the requested size of `nil` if the image could not be loaded.
+    public static func asset(named:String, atScale:CGFloat = 1.0) -> NSImage? {
+        
+        // Ensure we can load the image
+        guard let sourceImage = NSImage(named: named) else {
+            return nil
+        }
+        
+        // Are we at full size?
+        if atScale == 1.0 {
+            // Yes, just return image as-is
+            return sourceImage
+        }
+        
+        // Calculate new image size
+        let size = NSSize(width:sourceImage.size.width * atScale, height:sourceImage.size.height * atScale)
+        
+        // Return the resized image.
+        return NSImage.resizeImage(image: sourceImage, maxSize: size)
+    }
+    
+    /// Returns a `NSImage` from the Asset Catalog scaled to the requested size by the given width and height scale.
+    /// - Parameters:
+    ///   - named: The name of the image asset to load.
+    ///   - atWidthScale: [Optional] The requested width scale for the image. The default is `1.0`.
+    ///   - atHeightScale: [Optional] The requested height scale for the image. The default is `1.0`.
+    /// - Returns: The requested image asset scaled to the requested size of `nil` if the image could not be loaded.
+    public static func asset(named:String, atWidthScale:CGFloat = 1.0, atHeightScale:CGFloat = 1.0) -> NSImage? {
+        
+        // Ensure we can load the image
+        guard let sourceImage = NSImage(named: named) else {
+            return nil
+        }
+        
+        // Are we at full size?
+        if atWidthScale == 1.0 && atHeightScale == 1.0 {
+            // Yes, just return image as-is
+            return sourceImage
+        }
+        
+        // Calculate new image size
+        let size = NSSize(width:sourceImage.size.width * atWidthScale, height:sourceImage.size.height * atHeightScale)
+        
+        // Return the resized image.
+        return NSImage.resizeImage(image: sourceImage, maxSize: size)
+    }
+    
+    /// Returns a `NSImage` scaled to the requested size from a `Data` stream containing the image data.
+    /// - Parameters:
+    ///   - data: The `Data` stream containing the image data.
+    ///   - size: A `CGSize` object containing the requested `width` and `height` for the image.
+    ///   - scale: [Optional] The requested scale for the image. The default is `1.0`.
+    /// - Returns: The requested image scaled to the requested size of `nil` if the image could not be loaded.
+    public class func scaledImage(data:Data, to size: CGSize, scale:CGFloat = 1.0) -> NSImage? {
+        // Create source from data
+        let imageSourceOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+        guard let source = CGImageSourceCreateWithData(data as CFData, imageSourceOptions) else {
+            Debug.error(subsystem: "NSImageExtensions", category: "scaledImage", "ScaledImage: Source data for the image does not exist")
+            return nil
+        }
+        
+        // Calculate the desired dimension
+        let maxDimensionInPixels = max(size.width, size.height) * scale
+        
+        let options: [CFString: Any] = [
+                kCGImageSourceCreateThumbnailFromImageIfAbsent: true,
+                kCGImageSourceCreateThumbnailWithTransform: true,
+                kCGImageSourceShouldCacheImmediately: true,
+                kCGImageSourceThumbnailMaxPixelSize: maxDimensionInPixels
+            ]
+        
+        if let image = CGImageSourceCreateThumbnailAtIndex(source, 0, options as CFDictionary) {
+            return NSImage(cgImage: image, size: size)
+        } else {
+            return nil
+        }
+    }
+    
+    /// Returns a `NSImage` from the given URL scaled to the requested size.
+    /// - Parameters:
+    ///   - url: The URL of the source image.
+    ///   - size: A `CGSize` object containing the requested `width` and `height` for the image.
+    ///   - scale: [Optional] The requested scale for the image. The default is `1.0`.
+    /// - Returns: The requested image scaled to the requested size of `nil` if the image could not be loaded.
+    public class func scaledImage(url: String, to size: CGSize, scale:CGFloat = 1.0) -> NSImage? {
+        // Validate URL
+        guard let bundleURL = URL(string: url) else {
+            Debug.error(subsystem: "NSImageExtensions", category: "scaledImage", "ScaledImage: This image named \(url) does not exist")
+            return nil
+        }
+
+        // Validate data
+        guard let imageData = try? Data(contentsOf: bundleURL) else {
+            Debug.error(subsystem: "NSImageExtensions", category: "scaledImage", "ScaledImage: Cannot turn image named \(url) into NSData")
+            return nil
+        }
+
+        return scaledImage(data:imageData, to:size, scale: scale)
+    }
+    
+    /// Returns a `NSImage` from the given URL scaled to the requested size.
+    /// - Parameters:
+    ///   - url: The URL of the source image.
+    ///   - scale: [Optional] The requested scale for the image. The default is `1.0`.
+    /// - Returns: The requested image scaled to the requested size of `nil` if the image could not be loaded.
+    public class func scaledImage(url: String, scale:CGFloat = 1.0) -> NSImage? {
+        // Validate URL
+        guard let bundleURL = URL(string: url) else {
+            Debug.error(subsystem: "NSIImageExtensions", category: "scaledImage", "ScaledImage: This image named \(url) does not exist")
+            return nil
+        }
+
+        // Validate data
+        guard let imageData = try? Data(contentsOf: bundleURL) else {
+            Debug.error(subsystem: "NSImageExtensions", category: "scaledImage", "ScaledImage: Cannot turn image named \(url) into NSData")
+            return nil
+        }
+        
+        guard let size = sizeOfImageAt(url: bundleURL) else {
+            return nil
+        }
+
+        return scaledImage(data:imageData, to:size, scale: scale)
+    }
+    
+    /// Returns a `NSImage` with the given name from the Main App Bundle scaled to the requested size. This routine currently only works with `.jpg` and `.png` image types.
+    /// - Parameters:
+    ///   - name: The name of the image to load from the bundle.
+    ///   - size: A `CGSize` object containing the requested `width` and `height` for the image.
+    ///   - scale: [Optional] The requested scale for the image. The default is `1.0`.
+    /// - Returns: The requested image scaled to the requested size of `nil` if the image could not be loaded.
+    public class func scaledImage(name: String, to size: CGSize, scale:CGFloat = 1.0) -> NSImage? {
+        var bundleURL:URL? = nil
+        
+        // Try to load as jpg first
+        bundleURL = Bundle.main.url(forResource: name, withExtension: "jpg")
+        
+        // If not found, try png
+        if bundleURL == nil {
+            bundleURL = Bundle.main.url(forResource: name, withExtension: "png")
+        }
+        
+        guard let bundleURL = bundleURL else {
+            return nil
+        }
+        
+        // Validate data
+        guard let imageData = try? Data(contentsOf: bundleURL) else {
+            Debug.error(subsystem: "NSImageExtensions", category: "scaledImage", "ScaledImage: Cannot turn image named \(name) into NSData")
+            return nil
+        }
+        
+        return scaledImage(data:imageData, to:size, scale: scale)
+    }
+    
+    /// Returns the URL pointing to an image in the Main App Bundle with the given name and extension.
+    /// - Parameters:
+    ///   - name: The name of the image
+    ///   - withExtension: The extension of the image.
+    /// - Returns: The URL pointing to the requested image resource or `nil` if the image cannot be found.
+    public class func urlOfImageInBundle(name:String, withExtension:String) -> URL? {
+        
+        guard let path = Bundle.main.path(forResource: name, ofType: withExtension) else {
+            return nil
+        }
+        
+        return URL(fileURLWithPath: path)
+    }
+    
+    /// Returns a `NSImage` with the given name from the Main App Bundle scaled to the requested size.
+    /// - Parameters:
+    ///   - name: The name of the image to load.
+    ///   - scale: [Optional] The requested scale for the image. The default is `1.0`.
+    /// - Returns: The requested image scaled to the requested size of `nil` if the image could not be loaded.
+    public class func scaledImage(name: String, scale:CGFloat = 1.0) -> NSImage? {
+        var bundleURL:URL? = nil
+        
+        // Try to load as jpg first
+        bundleURL = Bundle.main.url(forResource: name, withExtension: "jpg")
+        
+        // If not found, try png
+        if bundleURL == nil {
+            bundleURL = Bundle.main.url(forResource: name, withExtension: "png")
+        }
+        
+        guard let bundleURL = bundleURL else {
+            return nil
+        }
+        
+        guard let size = sizeOfImageAt(url: bundleURL) else {
+            return nil
+        }
+        
+        // Validate data
+        guard let imageData = try? Data(contentsOf: bundleURL) else {
+            Debug.error(subsystem: "NSImageExtensions", category: "scaledImage", "ScaledImage: Cannot turn image named \(name) into NSData")
+            return nil
+        }
+        
+        return scaledImage(data:imageData, to:size, scale: scale)
+    }
+    
+    /// Returns the size of the image at the requested URL.
+    /// - Parameter url: The URL pointing to the requested image
+    /// - Returns: A `CGSize` containing the width and height of the requeste image or `nil` if the image cannot be found.
+    public static func sizeOfImageAt(url: URL) -> CGSize? {
+        // with CGImageSource we avoid loading the whole image into memory
+        guard let source = CGImageSourceCreateWithURL(url as CFURL, nil) else {
+            return nil
+        }
+        
+        let propertiesOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+        guard let properties = CGImageSourceCopyPropertiesAtIndex(source, 0, propertiesOptions) as? [CFString: Any] else {
+            return nil
+        }
+        
+        if let width = properties[kCGImagePropertyPixelWidth] as? CGFloat,
+           let height = properties[kCGImagePropertyPixelHeight] as? CGFloat {
+            return CGSize(width: width, height: height)
+        } else {
+            return nil
+        }
     }
 }
 #endif
