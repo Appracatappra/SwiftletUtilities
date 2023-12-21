35b92f3d0e5ea66ef3254dd1e812cdc8e61c3578
Kevin Mullins <kevin_mullins@appracatappra.com>
2023-12-21 07:05:00 -0600

    Image From Path
    Added the ability to load an image from either a Swift Package or App Bundle.

--- Sources/SwiftletUtilities/iOS/Extensions/iOS ImageExtensions.swift
+++ Sources/SwiftletUtilities/iOS/Extensions/iOS ImageExtensions.swift
@@ -128,6 +128,32 @@ extension Image {
     }
     
     // MARK: - Initializers
+    /// Fetches an image from the path to an item in a Swift Package or app Bundle.
+    /// - Parameter path: The path to the item to load.
+    init(path:String?) {
+        
+        guard let path else {
+            self.init("")
+            return
+        }
+        
+        #if canImport(UIKit)
+        guard let image = UIImage(contentsOfFile: path) else {
+            self.init("")
+            return
+        }
+        self.init(uiImage: image)
+        #elseif canImport(AppKit)
+        guard let image = NSImage(contentsOfFile: path) else {
+            self.init("")
+            return
+        }
+        self.init(nsImage: image)
+        #else
+        self.init("")
+        #endif
+    }
+    
     /**
      Creates a new `Image` instance from a Base 64 encoded string.
      
